ca1a9faf697cc58471bb9118117716aef809a3de
damiancupo1 <damiancupo@gmail.com>
2026-04-22 17:27:59 -0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import AppWithSupabase from './AppWithSupabase.tsx';
-import AuthWrapper from './components/AuthWrapper.tsx';
+import AppWithSupabase from './AppWithSupabase';
+import AuthWrapper from './components/AuthWrapper';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthWrapper>
-      {(session) => <AppWithSupabase />}
+      {(session) => <AppWithSupabase session={session} />}
     </AuthWrapper>
   </StrictMode>
 );
